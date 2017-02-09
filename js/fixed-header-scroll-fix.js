fa2f64eb8135192ccb53e98a66dfa6ed45ea24fc
Stephen Kent <smkent@smkent.net>
2017-02-08 22:59:51 -0800

    Add scroll offset when scrolling to an anchor present at page load time
    This takes Metrolook's fixed page header into account.
    
    https://phabricator.wikimedia.org/T157532

--- js/fixed-header-scroll-fix.js
+++ js/fixed-header-scroll-fix.js
@@ -1,3 +1,4 @@
+/* Add offset when scrolling to an anchor from a table of contents link */
 $('.toc ul a[href^=#]').on('click', function(e){
 	var href = $(this).attr('href');
 	var dest = $(href).offset().top - $('div.vectorMenu#usermenu').height();
@@ -5,3 +6,9 @@ $('.toc ul a[href^=#]').on('click', function(e){
 	$('html, body').scrollTop(dest);
 	e.preventDefault();
 });
+
+/* Add offset when scrolling to an anchor present at page load time */
+if ($(':target').length > 0) {
+    var d = $(':target').offset().top - $('div.vectorMenu#usermenu').height();
+    $('html, body').scrollTop(d);
+}
