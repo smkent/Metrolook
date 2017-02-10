c2c0e14b47ad0c3e54b27cac95e7c9fef804802d
Stephen Kent <smkent@smkent.net>
2017-02-09 18:46:16 -0800

    Increase the anchor scroll offset by 10 pixels

--- js/fixed-header-scroll-fix.js
+++ js/fixed-header-scroll-fix.js
@@ -1,7 +1,8 @@
 /* Add offset when scrolling to an anchor from a table of contents link */
 $('.toc ul a[href^=#]').on('click', function(e){
 	var href = $(this).attr('href');
-	var dest = $(href).offset().top - $('div.vectorMenu#usermenu').height();
+	var dest = ($(href).offset().top -
+		($('div.vectorMenu#usermenu').height() + 10));
 	window.history.pushState({}, '', $(this).prop('href'));
 	$('html, body').scrollTop(dest);
 	e.preventDefault();
@@ -9,6 +10,7 @@ $('.toc ul a[href^=#]').on('click', function(e){
 
 /* Add offset when scrolling to an anchor present at page load time */
 if ($(':target').length > 0) {
-    var d = $(':target').offset().top - $('div.vectorMenu#usermenu').height();
-    $('html, body').scrollTop(d);
+	var dest = ($(':target').offset().top -
+		($('div.vectorMenu#usermenu').height() + 10));
+	$('html, body').scrollTop(dest);
 }
